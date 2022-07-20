4c466b2866cbc242dfceb6e80989d154a16dd5ce
Nicoleta Cristea <n.cristea@uw.edu>
2022-07-20 13:23:16 +0300

    test spu fcn

--- hydroplotting/funcutils.py
+++ hydroplotting/funcutils.py
@@ -12,6 +12,9 @@ def calc_water_year(date):
 def calc_water_year_apply(df):
     df['water_year'] = df.datetime.apply(lambda row: calc_water_year(row))
 
+def calc_water_year_apply_SPU(df):
+    df['water_year'] = df.DateTime.apply(lambda row: calc_water_year(row))
+
 def snotel_data(path_snotel):
     snotel_obs = pd.read_csv(path_snotel, sep='\s+', header=None)
     snotel_obs.columns = ['year', 'month', 'day', 'daily_P_in', 'Tmax_F', 'Tmin_F', 'Tmean_F', 'SWE_in']
@@ -22,13 +25,10 @@ def snotel_data(path_snotel):
     calc_water_year_apply(snotel_obs)
     return snotel_obs
 
-def spu_snotel_data(path_snotel_spu):
-    snotel_obs = pd.read_csv(path_snotel, sep='\s+', header=None)
-    snotel_obs.columns = ['year', 'month', 'day', 'daily_P_in', 'Tmax_F', 'Tmin_F', 'Tmean_F', 'SWE_in']
-    #snotel_obs['Tmean_C'] = (snotel_obs['Tmean_F'] - 32) * (5 / 9)
-    snotel_obs = snotel_obs.set_index(pd.DatetimeIndex(pd.to_datetime(snotel_obs[['year', 'month', 'day']])))
-    #snotel_obs['SWE_m'] = snotel_obs['SWE_in'] * 0.0254
-    #snotel_obs['datetime'] = snotel_obs.index
-    #calc_water_year_apply(snotel_obs)
+def spu_snotel_data(path_SPU):
+    parse_dates = ['DateTime']
+    snotel_obs = pd.read_csv(path_SPU,  parse_dates=parse_dates)
+    snotel_obs = snotel_obs.set_index(snotel_obs['DateTime'])
+    calc_water_year_apply_SPU(snotel_obs)
     return snotel_obs
 #%%
